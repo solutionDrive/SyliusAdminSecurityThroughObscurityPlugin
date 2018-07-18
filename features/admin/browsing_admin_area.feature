febbfe188ce71720c587dcda1ab56d5fc34e8741
Tobias Lückel <lueckel@solutiondrive.de>
2018-07-18 10:28:05 +0200

    Extended the feature to also check sub menu entries

--- features/admin/browsing_admin_area.feature
+++ features/admin/browsing_admin_area.feature
@@ -9,10 +9,25 @@ Feature: Browsing admin area
     And I am logged in as an administrator
 
   @ui
-  Scenario: Browsing admin area with default admin role
+  Scenario: Browsing admin area with default admin role to see main menu
     When I open administration dashboard
     Then I should see "Catalog" menu entry
      And I should see "Sales" menu entry
      And I should see "Customer" menu entry
      And I should see "Marketing" menu entry
      And I should see "Configuration" menu entry
+
+  @ui
+  Scenario: Browsing admin area with default admin role to see sub menu
+    When I open administration dashboard
+    Then I should see "Catalog->Products" menu entry
+    Then I should see "Catalog->Options" menu entry
+    And I should see "Sales->Orders" menu entry
+    And I should see "Customer->Customers" menu entry
+    And I should see "Customer->Groups" menu entry
+    And I should see "Marketing->Promotions" menu entry
+    And I should see "Marketing->Product reviews" menu entry
+    And I should see "Configuration->Channels" menu entry
+    And I should see "Configuration->Currencies" menu entry
+    And I should see "Configuration->Locales" menu entry
+    And I should see "Configuration->Administrators" menu entry
--- tests/Behat/Page/Admin/MenuPage.php
+++ tests/Behat/Page/Admin/MenuPage.php
@@ -18,6 +18,10 @@ class MenuPage extends SymfonyPage implements MenuPageInterface
     /**
      * {@inheritdoc}
      */
+    private const MAIN_ENTRY = 'div.menu div.item div.header';
+
+    private const SUB_ENTRY = 'div.menu a.item';
+
     public function getRouteName()
     {
         # The menu does not have an own route
@@ -37,13 +41,56 @@ class MenuPage extends SymfonyPage implements MenuPageInterface
 
     protected function getDefinedElements()
     {
-        return array_merge(parent::getDefinedElements(), [
-            'Catalog' => 'div.menu div.item div.header:contains("Catalog")',
-            'Sales' => 'div.menu div.item div.header:contains("Sales")',
-            'Customer' => 'div.menu div.item div.header:contains("Customer")',
-            'Marketing' => 'div.menu div.item div.header:contains("Marketing")',
-            'Configuration' => 'div.menu div.item div.header:contains("Configuration")',
-        ]);
+        $catalogMenuElements = [
+            'Catalog'                       => self::MAIN_ENTRY . ':contains("Catalog")',
+            'Catalog->Taxons'               => self::MAIN_ENTRY . ':contains("Catalog") + ' . self::SUB_ENTRY . ':contains("Taxons")',
+            'Catalog->Products'             => self::MAIN_ENTRY . ':contains("Catalog") + ' . self::SUB_ENTRY . ':contains("Products")',
+            'Catalog->Inventory'            => self::MAIN_ENTRY . ':contains("Catalog") + ' . self::SUB_ENTRY . ':contains("Inventory")',
+            'Catalog->Attributes'           => self::MAIN_ENTRY . ':contains("Catalog") + ' . self::SUB_ENTRY . ':contains("Attributes")',
+            'Catalog->Options'              => self::MAIN_ENTRY . ':contains("Catalog") + ' . self::SUB_ENTRY . ':contains("Options")',
+        ];
+
+        $salesMenuElements = [
+            'Sales'         => self::MAIN_ENTRY . ':contains("Sales")',
+            'Sales->Orders' => self::MAIN_ENTRY . ':contains("Sales") + ' . self::SUB_ENTRY . ':contains("Orders")',
+        ];
+
+        $customerMenuElements = [
+            'Customer'              => self::MAIN_ENTRY . ':contains("Customer")',
+            'Customer->Customers'   => self::MAIN_ENTRY . ':contains("Customer") + ' . self::SUB_ENTRY . ':contains("Customers")',
+            'Customer->Groups'      => self::MAIN_ENTRY . ':contains("Customer") + ' . self::SUB_ENTRY . ':contains("Groups")',
+        ];
+
+        $marketingMenuElements = [
+            'Marketing'                     => self::MAIN_ENTRY . ':contains("Marketing")',
+            'Marketing->Promotions'         => self::MAIN_ENTRY . ':contains("Marketing") + ' . self::SUB_ENTRY . ':contains("Promotions")',
+            'Marketing->Product reviews'    => self::MAIN_ENTRY . ':contains("Marketing") + ' . self::SUB_ENTRY . ':contains("Product reviews")',
+        ];
+
+        $configurationMenuElements = [
+            'Configuration'                         => self::MAIN_ENTRY . ':contains("Configuration")',
+            'Configuration->Channels'               => self::MAIN_ENTRY . ':contains("Configuration") + ' . self::SUB_ENTRY . ':contains("Channels")',
+            'Configuration->Countries'              => self::MAIN_ENTRY . ':contains("Configuration") + ' . self::SUB_ENTRY . ':contains("Countries")',
+            'Configuration->Zones'                  => self::MAIN_ENTRY . ':contains("Configuration") + ' . self::SUB_ENTRY . ':contains("Zones")',
+            'Configuration->Currencies'             => self::MAIN_ENTRY . ':contains("Configuration") + ' . self::SUB_ENTRY . ':contains("Currencies")',
+            'Configuration->Exchange rates'         => self::MAIN_ENTRY . ':contains("Configuration") + ' . self::SUB_ENTRY . ':contains("Exchange rates")',
+            'Configuration->Locales'                => self::MAIN_ENTRY . ':contains("Configuration") + ' . self::SUB_ENTRY . ':contains("Locales")',
+            'Configuration->Payment methods'        => self::MAIN_ENTRY . ':contains("Configuration") + ' . self::SUB_ENTRY . ':contains("Payment methods")',
+            'Configuration->Shipping methods'       => self::MAIN_ENTRY . ':contains("Configuration") + ' . self::SUB_ENTRY . ':contains("Shipping methods")',
+            'Configuration->Shipping categories'    => self::MAIN_ENTRY . ':contains("Configuration") + ' . self::SUB_ENTRY . ':contains("Shipping categories")',
+            'Configuration->Tax categories'         => self::MAIN_ENTRY . ':contains("Configuration") + ' . self::SUB_ENTRY . ':contains("Tax categories")',
+            'Configuration->Tax rates'              => self::MAIN_ENTRY . ':contains("Configuration") + ' . self::SUB_ENTRY . ':contains("Tax rates")',
+            'Configuration->Administrators'         => self::MAIN_ENTRY . ':contains("Configuration") + ' . self::SUB_ENTRY . ':contains("Administrators")',
+        ];
+
+        return array_merge(
+            parent::getDefinedElements(),
+            $catalogMenuElements,
+            $salesMenuElements,
+            $customerMenuElements,
+            $marketingMenuElements,
+            $configurationMenuElements
+        );
     }
 }
 
